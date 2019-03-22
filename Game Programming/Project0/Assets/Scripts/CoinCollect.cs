3c8086afc41b5f6de16afdef919a4c590875c0af
Howard Jiang <Howard.Jiang76@myhunter.cuny.edu>
2019-03-21 20:07:55 -0400

    2D game
    We had to create a game from scratch on unity for our first project, this is mine

---
+++ Game Programming/Project0/Assets/Scripts/CoinCollect.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CoinCollect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int score = 0;
+    public Text scoreText;
+    public Slider progressBar;
+
+    void Start()
+    {
+        scoreText.text = "Progress: " + score.ToString() + "/10" ;
+        progressBar.value = score;
+    }
+
+    public void NextScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            score++;
+            progressBar.value = score;
+            if (score == 10)
+            {
+                SceneManager.LoadScene("Level2");
+            }
+            else
+            {
+                scoreText.text = "Progress: " + score.ToString() + "/10";
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
---
+++ Game Programming/Project0/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float moveInput;
+    private Rigidbody2D rb;
+    private bool faceR = true;
+    private bool isGrounded;
+
+    public float speed = 10;
+    public float jumpForce = 10;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    public float checkRadius = 1f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        //Debug.Log(isGrounded); //shows false?
+        //is circle positioned around groundcheck, if radius is touching what is ground. 
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        if (faceR == true && moveInput < 0)
+        {
+            Flip();
+        }
+        else if (faceR == false && moveInput > 0)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        faceR = !faceR;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+}
